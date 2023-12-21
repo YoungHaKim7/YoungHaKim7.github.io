7efe42a179a9b9853ca03cffc824c7ff557d6532
GYoung <ytok1108@aol.com>
2023-12-21 22:34:34 +0900

    Rust Yew ToDo App

--- src/main.rs
+++ src/main.rs
@@ -1,151 +1,249 @@
-use chrono::{Local, TimeZone, Utc};
-use gloo::console::{self, Timer};
-use gloo::timers::callback::{Interval, Timeout};
-use yew::{html, Component, Context, Html};
+use gloo::storage::{LocalStorage, Storage};
+use state::{Entry, Filter, State};
+use strum::IntoEnumIterator;
+use web_sys::HtmlInputElement as InputElement;
+use yew::events::{FocusEvent, KeyboardEvent};
+use yew::html::Scope;
+use yew::{classes, html, Classes, Component, Context, Html, NodeRef, TargetCast};
+
+mod state;
+
+const KEY: &str = "yew.todomvc.self";
 
 pub enum Msg {
-    StartTimeout,
-    StartInterval,
-    Cancel,
-    Done,
-    Tick,
-    UpdateTime,
+    Add(String),
+    Edit((usize, String)),
+    Remove(usize),
+    SetFilter(Filter),
+    ToggleAll,
+    ToggleEdit(usize),
+    Toggle(usize),
+    ClearCompleted,
+    Focus,
 }
 
 pub struct App {
-    time: String,
-    messages: Vec<&'static str>,
-    _standalone: (Interval, Interval),
-    interval: Option<Interval>,
-    timeout: Option<Timeout>,
-    console_timer: Option<Timer<'static>>,
-}
-
-impl App {
-    fn get_current_time() -> String {
-        let date = js_sys::Date::new_0();
-        let utc_date = chrono::Utc::now();
-        let utc_date_str = utc_date.to_string();
-        String::from(utc_date_str)
-    }
-
-    fn cancel(&mut self) {
-        self.timeout = None;
-        self.interval = None;
-    }
+    state: State,
+    focus_ref: NodeRef,
 }
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
-        let standalone_handle =
-            Interval::new(10, || console::debug!("Example of a standalone callback."));
-
-        let clock_handle = {
-            let link = ctx.link().clone();
-            Interval::new(1, move || link.send_message(Msg::UpdateTime))
+    fn create(_ctx: &Context<Self>) -> Self {
+        let entries = LocalStorage::get(KEY).unwrap_or_else(|_| Vec::new());
+        let state = State {
+            entries,
+            filter: Filter::All,
+            edit_value: "".into(),
         };
-
-        Self {
-            time: App::get_current_time(),
-            messages: Vec::new(),
-            _standalone: (standalone_handle, clock_handle),
-            interval: None,
-            timeout: None,
-            console_timer: None,
-        }
+        let focus_ref = NodeRef::default();
+        Self { state, focus_ref }
     }
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::StartTimeout => {
-                let handle = {
-                    let link = ctx.link().clone();
-                    Timeout::new(3, move || link.send_message(Msg::Done))
-                };
-
-                self.timeout = Some(handle);
-
-                self.messages.clear();
-                console::clear!();
-
-                self.messages.push("Timer started!");
-                self.console_timer = Some(Timer::new("Timer"));
-                true
-            }
-            Msg::StartInterval => {
-                let handle = {
-                    let link = ctx.link().clone();
-                    Interval::new(1, move || link.send_message(Msg::Tick))
-                };
-                self.interval = Some(handle);
-
-                self.messages.clear();
-                console::clear!();
-
-                self.messages.push("Interval started!");
-                true
-            }
-            Msg::Cancel => {
-                self.cancel();
-                self.messages.push("Canceled!");
-                console::warn!("Canceled!");
-                true
-            }
-            Msg::Done => {
-                self.cancel();
-                self.messages.push("Done!");
-
-                // todo weblog
-                // ConsoleService::group();
-                console::info!("Done!");
-                if let Some(timer) = self.console_timer.take() {
-                    drop(timer);
+            Msg::Add(description) => {
+                let description = description.trim();
+                if !description.is_empty() {
+                    let entry = Entry {
+                        description: description.to_string(),
+                        completed: false,
+                        editing: false,
+                    };
+                    self.state.entries.push(entry);
                 }
-
-                // todo weblog
-                // ConsoleService::group_end();
-                true
             }
-            Msg::Tick => {
-                self.messages.push("Tick...");
-                // todo weblog
-                // ConsoleService::count_named("Tick");
-                true
+            Msg::Edit((idx, edit_value)) => {
+                self.state.complete_edit(idx, edit_value.trim().to_string());
+                self.state.edit_value = "".to_string();
             }
-            Msg::UpdateTime => {
-                self.time = App::get_current_time();
-                true
+            Msg::Remove(idx) => {
+                self.state.remove(idx);
+            }
+            Msg::SetFilter(filter) => {
+                self.state.filter = filter;
+            }
+            Msg::ToggleEdit(idx) => {
+                let entry = self
+                    .state
+                    .entries
+                    .iter()
+                    .filter(|e| self.state.filter.fits(e))
+                    .nth(idx)
+                    .unwrap();
+                self.state.edit_value = entry.description.clone();
+                self.state.clear_all_edit();
+                self.state.toggle_edit(idx);
+            }
+            Msg::ToggleAll => {
+                let status = !self.state.is_all_completed();
+                self.state.toggle_all(status);
+            }
+            Msg::Toggle(idx) => {
+                self.state.toggle(idx);
+            }
+            Msg::ClearCompleted => {
+                self.state.clear_completed();
+            }
+            Msg::Focus => {
+                if let Some(input) = self.focus_ref.cast::<InputElement>() {
+                    input.focus().unwrap();
+                }
             }
         }
+        LocalStorage::set(KEY, &self.state.entries).expect("failed to set");
+        true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let has_job = self.timeout.is_some() || self.interval.is_some();
+        let hidden_class = if self.state.entries.is_empty() {
+            "hidden"
+        } else {
+            ""
+        };
         html! {
-            <>
-                <div id="buttons">
-                    <button disabled={has_job} onclick={ctx.link().callback(|_| Msg::StartTimeout)}>
-                        { "Start Timeout" }
-                    </button>
-                    <button disabled={has_job} onclick={ctx.link().callback(|_| Msg::StartInterval)}>
-                        { "Start Interval" }
-                    </button>
-                    <button disabled={!has_job} onclick={ctx.link().callback(|_| Msg::Cancel)}>
-                        { "Cancel!" }
-                    </button>
+            <div class="todomvc-wrapper">
+                <section class="todoapp">
+                    <header class="header">
+                        <h1>{ "todos" }</h1>
+                        { self.view_input(ctx.link()) }
+                    </header>
+                    <section class={classes!("main", hidden_class)}>
+                        <input
+                            type="checkbox"
+                            class="toggle-all"
+                            id="toggle-all"
+                            checked={self.state.is_all_completed()}
+                            onclick={ctx.link().callback(|_| Msg::ToggleAll)}
+                        />
+                        <label for="toggle-all" />
+                        <ul class="todo-list">
+                            { for self.state.entries.iter().filter(|e| self.state.filter.fits(e)).enumerate().map(|e| self.view_entry(e, ctx.link())) }
+                        </ul>
+                    </section>
+                    <footer class={classes!("footer", hidden_class)}>
+                        <span class="todo-count">
+                            <strong>{ self.state.total() }</strong>
+                            { " item(s) left" }
+                        </span>
+                        <ul class="filters">
+                            { for Filter::iter().map(|flt| self.view_filter(flt, ctx.link())) }
+                        </ul>
+                        <button class="clear-completed" onclick={ctx.link().callback(|_| Msg::ClearCompleted)}>
+                            { format!("Clear completed ({})", self.state.total_completed()) }
+                        </button>
+                    </footer>
+                </section>
+                <footer class="info">
+                    <p>{ "Double-click to edit a todo" }</p>
+                    <p>{ "Written by " }<a href="https://github.com/DenisKolodin/" target="_blank">{ "Denis Kolodin" }</a></p>
+                    <p>{ "Part of " }<a href="http://todomvc.com/" target="_blank">{ "TodoMVC" }</a></p>
+                </footer>
+            </div>
+        }
+    }
+}
+
+impl App {
+    fn view_filter(&self, filter: Filter, link: &Scope<Self>) -> Html {
+        let cls = if self.state.filter == filter {
+            "selected"
+        } else {
+            "not-selected"
+        };
+        html! {
+            <li>
+                <a class={cls}
+                   href={filter.as_href()}
+                   onclick={link.callback(move |_| Msg::SetFilter(filter))}
+                >
+                    { filter }
+                </a>
+            </li>
+        }
+    }
+
+    fn view_input(&self, link: &Scope<Self>) -> Html {
+        let onkeypress = link.batch_callback(|e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let input: InputElement = e.target_unchecked_into();
+                let value = input.value();
+                input.set_value("");
+                Some(Msg::Add(value))
+            } else {
+                None
+            }
+        });
+        html! {
+            // You can use standard Rust comments. One line:
+            // <li></li>
+            <input
+                class="new-todo"
+                placeholder="What needs to be done?"
+                {onkeypress}
+            />
+            /* Or multiline:
+            <ul>
+                <li></li>
+            </ul>
+            */
+        }
+    }
+
+    fn view_entry(&self, (idx, entry): (usize, &Entry), link: &Scope<Self>) -> Html {
+        let mut class = Classes::from("todo");
+        if entry.editing {
+            class.push(" editing");
+        }
+        if entry.completed {
+            class.push(" completed");
+        }
+        html! {
+            <li {class}>
+                <div class="view">
+                    <input
+                        type="checkbox"
+                        class="toggle"
+                        checked={entry.completed}
+                        onclick={link.callback(move |_| Msg::Toggle(idx))}
+                    />
+                    <label ondblclick={link.callback(move |_| Msg::ToggleEdit(idx))}>{ &entry.description }</label>
+                    <button class="destroy" onclick={link.callback(move |_| Msg::Remove(idx))} />
                 </div>
-                <div id="wrapper">
-                    <div id="time">
-                        { &self.time }
-                    </div>
-                    <div id="messages">
-                        { for self.messages.iter().map(|message| html! { <p>{ message }</p> }) }
-                    </div>
-                </div>
-            </>
+                { self.view_entry_edit_input((idx, entry), link) }
+            </li>
+        }
+    }
+
+    fn view_entry_edit_input(&self, (idx, entry): (usize, &Entry), link: &Scope<Self>) -> Html {
+        let edit = move |input: InputElement| {
+            let value = input.value();
+            input.set_value("");
+            Msg::Edit((idx, value))
+        };
+
+        let onblur = link.callback(move |e: FocusEvent| edit(e.target_unchecked_into()));
+
+        let onkeypress = link.batch_callback(move |e: KeyboardEvent| {
+            (e.key() == "Enter").then(|| edit(e.target_unchecked_into()))
+        });
+
+        if entry.editing {
+            html! {
+                <input
+                    class="edit"
+                    type="text"
+                    ref={self.focus_ref.clone()}
+                    value={self.state.edit_value.clone()}
+                    onmouseover={link.callback(|_| Msg::Focus)}
+                    {onblur}
+                    {onkeypress}
+                />
+            }
+        } else {
+            html! { <input type="hidden" /> }
         }
     }
 }
