52bcfc48f8d7d14206c0afa3df20bb9e76ca85f2
GYoung <ytok1108@aol.com>
2024-11-04 22:19:36 +0900

    summer time 종료

--- src/js_src/clock.js
+++ src/js_src/clock.js
@@ -7,17 +7,17 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2, "0");
     const KrDate = new Date().toLocaleString(undefined, { year: 'numeric', month: '2-digit', day: '2-digit', weekday: "long", hour: '2-digit', hour12: false, minute: '2-digit' });
     const NyDate_summer = new Date(date.getTime() - date.getTimezoneOffset() * -26667).toISOString().split("T")[0];
-    const NyTime1_summer = new Date(date.getTime() - date.getTimezoneOffset() * -26667).toISOString().split("T")[1].slice(0,8);
+    const NyTime1_summer = new Date(date.getTime() - date.getTimezoneOffset() * -26667).toISOString().split("T")[1].slice(0, 8);
     const NyDate_winter = new Date(date.getTime() - date.getTimezoneOffset() * -33334).toISOString().split("T")[0];
-    const NyTime1_winter = new Date(date.getTime() - date.getTimezoneOffset() * -33334).toISOString().split("T")[1].slice(0,8);
-    const EUTime1_summer = new Date(date.getTime() - date.getTimezoneOffset() * 13333).toISOString().split("T")[1].slice(0,8);
-    const EUTime1_winter = new Date(date.getTime() - date.getTimezoneOffset() * 6666).toISOString().split("T")[1].slice(0,8);
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-    // Nyclock.innerText =  `NewYork🇺🇸(EST) \n${NyDate_winter} ⏰ ${NyTime1_winter}`;
-    // Euclock.innerText =  `Germany🇩🇪(EU) \n⏰ ${EUTime1_winter}`;
-    Nyclock.innerText =  `NewYork🇺🇸(EST) \n${NyDate_summer} ⏰ ${NyTime1_summer}`;
-    Euclock.innerText =  `Germany🇩🇪(EU) \n⏰ ${EUTime1_summer}`;
+    const NyTime1_winter = new Date(date.getTime() - date.getTimezoneOffset() * -33334).toISOString().split("T")[1].slice(0, 8);
+    const EUTime1_summer = new Date(date.getTime() - date.getTimezoneOffset() * 13333).toISOString().split("T")[1].slice(0, 8);
+    const EUTime1_winter = new Date(date.getTime() - date.getTimezoneOffset() * 6666).toISOString().split("T")[1].slice(0, 8);
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    Nyclock.innerText = `NewYork🇺🇸(EST) \n${NyDate_winter} ⏰ ${NyTime1_winter}`;
+    Euclock.innerText = `Germany🇩🇪(EU) \n⏰ ${EUTime1_winter}`;
+    // Nyclock.innerText =  `NewYork🇺🇸(EST) \n${NyDate_summer} ⏰ ${NyTime1_summer}`;
+    // Euclock.innerText =  `Germany🇩🇪(EU) \n⏰ ${EUTime1_summer}`;
     clock.innerText = `한국Korea🇰🇷 \n${KrDate}`;
 }
 
